35a266c5394ef492777c2ca83b9aa78c888e672f
Byte1024 <darkbyte7@live.com>
2019-01-02 01:40:03 -0600

    Update summary

--- VisualPlus/Structure/RECT.cs
+++ VisualPlus/Structure/RECT.cs
@@ -6,7 +6,7 @@
 // VisualPlus - The VisualPlus Framework (VPF) for WinForms .NET development.
 // 
 // Created: 10/12/2018 - 11:45 PM
-// Last Modified: 02/01/2019 - 1:24 AM
+// Last Modified: 02/01/2019 - 1:39 AM
 // 
 // Copyright (c) 2016-2019 VisualPlus <https://darkbyte7.github.io/VisualPlus/>
 // All Rights Reserved.
@@ -56,7 +56,7 @@ namespace VisualPlus.Structure
     {
         #region Static Fields
 
-        /// <summary>Represents a <see cref="RECT" /> structure with its properties left uninitialized.</summary>
+        /// <summary>Represents a <see cref="RECT" /> structure with no position or area.</summary>
         public static readonly RECT Empty;
 
         #endregion

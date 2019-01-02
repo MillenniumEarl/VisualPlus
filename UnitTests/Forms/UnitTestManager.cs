88fcc55b0477e5936519ca2720dc56e5c99fda3e
Byte1024 <darkbyte7@live.com>
2019-01-01 22:19:47 -0600

    Update Resharper Settings - Upload test

--- UnitTests/Forms/UnitTestManager.cs
+++ UnitTests/Forms/UnitTestManager.cs
@@ -1,4 +1,47 @@
-﻿#region Namespace
+﻿#region License
+
+// -----------------------------------------------------------------------------------------------------------
+// 
+// Project: UnitTests
+// File Name: UnitTestManager.cs
+// 
+// Description: The VisualPlus Framework (VPF) for WinForms .NET development.
+// 
+// Created: 10/12/2018 - 11:45 PM
+// Last Modified: 01/01/2019 - 10:15 PM
+// 
+// Copyright (c) 2016-2019 VisualPlus (https://darkbyte7.github.io/VisualPlus/)
+// All Rights Reserved.
+// 
+// -----------------------------------------------------------------------------------------------------------
+// 
+// GNU General Public License v3.0 (GPL-3.0)
+// 
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+// EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//  
+// This file is subject to the terms and conditions defined in the file 
+// 'LICENSE.md', which should be in the root directory of the source code package.
+// 
+// -----------------------------------------------------------------------------------------------------------
+
+#endregion
+
+#region Namespace
 
 using System;
 using System.Diagnostics;
@@ -23,11 +66,17 @@ namespace UnitTests.Forms
     /// <summary>The test manager.</summary>
     public partial class UnitTestManager : VisualForm
     {
+        #region Fields
+
         #region Variables
 
         private UnitTests _unitTest;
 
         #endregion
+
+        #endregion
+
+        #region Constructors and Destructors
 
         #region Constructors
 
@@ -40,6 +89,10 @@ namespace UnitTests.Forms
         }
 
         #endregion
+
+        #endregion
+
+        #region Enums
 
         #region Enumerators
 
@@ -70,6 +123,8 @@ namespace UnitTests.Forms
             /// <summary>The clipboard test.</summary>
             ClipboardTest = 7
         }
+
+        #endregion
 
         #endregion
 

a2acf7b713632741e476e2fddd6c9c70d75537d9
Byte1024 <darkbyte7@live.com>
2019-01-01 22:45:32 -0600

    Updated License Header

--- UnitTests/Forms/UnitTestManager.cs
+++ UnitTests/Forms/UnitTestManager.cs
@@ -2,15 +2,13 @@
 
 // -----------------------------------------------------------------------------------------------------------
 // 
-// Project: UnitTests
-// File Name: UnitTestManager.cs
-// 
-// Description: The VisualPlus Framework (VPF) for WinForms .NET development.
+// Name: UnitTestManager.cs
+// UnitTests - The VisualPlus Framework (VPF) for WinForms .NET development.
 // 
 // Created: 10/12/2018 - 11:45 PM
-// Last Modified: 01/01/2019 - 10:15 PM
+// Last Modified: 01/01/2019 - 10:43 PM
 // 
-// Copyright (c) 2016-2019 VisualPlus (https://darkbyte7.github.io/VisualPlus/)
+// Copyright (c) 2016-2019 VisualPlus <https://darkbyte7.github.io/VisualPlus/>
 // All Rights Reserved.
 // 
 // -----------------------------------------------------------------------------------------------------------

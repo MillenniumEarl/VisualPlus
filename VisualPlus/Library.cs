d9865196d0dc03a531946f352c161e68ddd8ced9
DarkByte7 <darkbyte7@live.com>
2019-01-17 19:12:31 -0600

    Add: API Info

---
+++ VisualPlus/Library.cs
@@ -0,0 +1,42 @@
+﻿#region Namespace
+
+using System.Reflection;
+
+#endregion
+
+namespace VisualPlus
+{
+    /// <summary>A collection of the <see cref="VisualPlus"/> API information.</summary>
+    public class Library
+    {
+        #region Static Fields
+
+        public static string Name = "VisualPlus";
+        public static string ProjectUrl = "https://github.com/DarkByte7/VisualPlus";
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>Returns the <c>AssemblyVersion</c> of this Library.</summary>
+        public static string Version
+        {
+            get
+            {
+                try
+                {
+                    Assembly assembly = Assembly.GetExecutingAssembly();
+                    AssemblyName assemblyName = assembly.GetName();
+
+                    return assemblyName.Version.ToString();
+                }
+                catch
+                {
+                    return "0.0.0.0";
+                }
+            }
+        }
+
+        #endregion
+    }
+}

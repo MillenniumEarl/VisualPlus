ec280a86f1be65ab52a145e87e14046c6b2aa8b8
DarkByte7 <darkbyte7@live.com>
2019-02-02 23:56:07 -0600

    Minor rename

--- VisualPlus/Extensibility/KeysExtensions.cs
+++ VisualPlus/Extensibility/KeysExtensions.cs
@@ -52,7 +52,7 @@ namespace VisualPlus.Extensibility
         /// <summary>Converts the specified <see cref="Keys" /> codes and modifiers type to a <see cref="int" />.</summary>
         /// <param name="key">Specified key codes and modifiers.</param>
         /// <returns>The <see cref="int" />.</returns>
-        public static int GetKeyCode(this Keys key)
+        public static int ToKeyCode(this Keys key)
         {
             return Convert.ToInt32(key);
         }
@@ -60,7 +60,7 @@ namespace VisualPlus.Extensibility
         /// <summary>Converts the specified <see cref="int" /> key code to a <see cref="Keys" />.</summary>
         /// <param name="keyCode">The value to convert to an enumeration member.</param>
         /// <returns>The <see cref="Keys" />.</returns>
-        public static Keys ToKey(this int keyCode)
+        public static Keys ToKeys(this int keyCode)
         {
             return (Keys)Enum.ToObject(typeof(Keys), keyCode);
         }
